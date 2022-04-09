7eaa501a9165df1ba0ef23048286a3082d2518ba
akhialomgir <akhialomgir362856@gmail.com>
2022-04-09 22:37:50 +0800

    update renew.py

--- renew.py
+++ renew.py
@@ -1,14 +1,7 @@
+from nacl import encoding, public
+from base64 import b64encode
 import os
 import requests
-
-user = os.environ['user'].split(';')
-session = requests.session()
-login = session.post("https://iray.club/api/v1/passport/auth/login", data={
-    "email": user[0],
-    "password": user[1]
-})
-if login.status_code != 200:
-    exit(login.status_code)
 
 
 def get_link() -> str:
@@ -20,5 +13,23 @@ def change_link() -> str:
     reset = session.get("https://iray.club/api/v1/user/resetSecurity")
     return reset.json()["data"]
 
-change_link()
+
+def encrypt(public_key: str, secret_value: str) -> str:
+    """Encrypt a Unicode string using the public key."""
+    public_key = public.PublicKey(
+        public_key.encode("utf-8"), encoding.Base64Encoder())
+    sealed_box = public.SealedBox(public_key)
+    encrypted = sealed_box.encrypt(secret_value.encode("utf-8"))
+    return b64encode(encrypted).decode("utf-8")
+
+
+user = os.environ['user'].split(';')
+session = requests.session()
+login = session.post("https://iray.club/api/v1/passport/auth/login", data={
+    "email": user[0],
+    "password": user[1]
+})
+if login.status_code != 200:
+    exit(login.status_code)
+link = change_link()
 
