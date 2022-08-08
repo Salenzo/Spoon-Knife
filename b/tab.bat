b5b21ae5ede0e01c341ecf937cc20f9c04110f5f
Frog Chen <satgo1546@yeah.net>
2022-08-08 21:37:49 +0800

    Update tab.bat

--- b/tab.bat
+++ b/tab.bat
@@ -1,4 +1,6 @@
 for /f "usebackq delims=" %%i in (`vswhere.exe -latest -property installationPath`) do %comspec% /k "%%i\VC\Auxiliary\Build\vcvars32.bat"
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DLAF_BACKEND=skia -DSKIA_DIR=..\c -DSKIA_LIBRARY_DIR=..\c\out\Release-x86 -DSKIA_LIBRARY=..\c\out\Release-x86\skia.lib -G Ninja ..\a
+cmake -DCMAKE_SYSTEM_VERSION=10.0.22621.0 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DLAF_BACKEND=skia ^
+  -DSKIA_DIR=..\c -DSKIA_LIBRARY_DIR=..\c\out\Release-x86 -DSKIA_LIBRARY=..\c\out\Release-x86\skia.lib ^
+  -G Ninja ..\a
 ninja aseprite
 7z a -psalenzo ..\d.7z .
