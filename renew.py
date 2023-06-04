fdfd34835411c0a735f177bf5adda7c284f27a03
akhialomgir <akhialomgir362856@gmail.com>
2023-06-04 17:11:51 +0800

    add baseUrl

--- renew.py
+++ renew.py
@@ -6,12 +6,12 @@ import json
 
 
 def get_link() -> str:
-    subscribe = session.get("https://scc.ustc.edu.rs/api/v1/user/getSubscribe")
+    subscribe = session.get(os.environ['baseUrl'] + "api/v1/user/getSubscribe")
     return subscribe.json()["data"]["subscribe_url"]
 
 
 def change_link() -> str:
-    reset = session.get("https://scc.ustc.edu.rs/api/v1/user/resetSecurity")
+    reset = session.get(os.environ['baseUrl'] + "api/v1/user/resetSecurity")
     return reset.json()["data"]
 
 
@@ -23,22 +23,43 @@ def encrypt(public_key: str, secret_value: str) -> str:
     encrypted = sealed_box.encrypt(secret_value.encode("utf-8"))
     return b64encode(encrypted).decode("utf-8")
 
+# dg lu
 user = os.environ['user'].split(';')
 session = requests.session()
-login = session.post("https://scc.ustc.edu.rs/api/v1/passport/auth/login", data={
+login = session.post(os.environ['baseUrl'] + "api/v1/passport/auth/login", data={
     "email": user[0],
     "password": user[1]
 })
-if login.status_code != 200:
-    exit(login.status_code)
-"""os.environ['url'] = change_link()"""  # 这个只会影响当前进程，py退出后就没了
-url = change_link()
 
 token = os.environ['token']
 public_key = json.loads(requests.get("https://api.github.com/repos/Salenzo/Spoon-Knife/actions/secrets/public-key", headers={
     "Authorization": f"token {token}",
     "Accept": "application/vnd.github.v3+json",
 }).content)
+
+# yi ih, is xn ho qu baseUrl
+if login.status_code != 200:
+    from bs4 import BeautifulSoup
+    response = requests.get("https://xn--zuup71g88ae4i.com/")
+    soup = BeautifulSoup(response.content, "html.parser")
+    for a in soup.find_all("a"):
+        if a.text.strip().startswith("立即进入"):
+            print(a.href)
+            baseUrl = a.href
+    print(requests.put("https://api.github.com/repos/Salenzo/Spoon-Knife/actions/secrets/BASE_URL", headers={
+        "Authorization": f"token {token}",
+        "Accept": "application/vnd.github.v3+json",
+        "Content-Type": "application/json",
+    }, data=json.dumps({
+        "key_id": public_key["key_id"],
+        "encrypted_value": encrypt(public_key["key"], baseUrl)
+    })).content, "gg xn baseUrl")
+    exit(login.status_code)
+
+"""os.environ['url'] = change_link()"""  # 这个只会影响当前进程，py退出后就没了
+url = change_link()
+
+# gg xn
 print(requests.put("https://api.github.com/repos/Salenzo/Spoon-Knife/actions/secrets/URL", headers={
     "Authorization": f"token {token}",
     "Accept": "application/vnd.github.v3+json",
